c63307d2a9bc8c2bb17d3a48a348524535a98944
rodolfotiago <rodolfotiago@gmail.com>
2019-03-30 22:34:09 -0300

    vai ter que ser com ubuntu mesmo por enquanto

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 docker build -t flutter-test .
-docker run -it -v /home/thaizita11/dker-flutter/odroid:/root/odroid/ -w /root/ --rm flutter-test flutter create odroid
-docker run -it -v /home/thaizita11/dker-flutter/odroid:/root/odroid/ -w /root/odroid --rm flutter-test flutter test
+docker run -it -v $PWD/odroid:/root/odroid/ -w /root/ --rm flutter-test flutter create odroid
+docker run -it -v $PWD/odroid:/root/odroid/ -w /root/odroid --rm flutter-test flutter test
