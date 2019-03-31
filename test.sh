1551e32b4b28d7c9477f84b1ab2e9d0198d221af
dodopontocom <rodolfotiago@gmail.com>
2019-03-31 19:53:14 -0300

    first consistent version

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 #
 #set -x
 
+IMAGE=flutter-android-sdk
 project_name=$1
 create=''
 
@@ -20,15 +21,15 @@ else
 	fi		
 fi
 
-docker build -t flutter-ubuntu -f Dockerfile-ubuntu .
+docker build -t $IMAGE -f Dockerfile .
 
 if [[ $create == yes ]]; then
-	docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/ flutter-ubuntu flutter create $project_name
+	docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/ $IMAGE flutter create $project_name
 fi
 
 
 echo "testing the app..."
-docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name flutter-ubuntu flutter test
+docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name $IMAGE flutter test
 
 echo "creating an alias for you"
 echo "next time you can simply run 'flutter <commands>'"
