a2348ce5aa3ce2045f94594e32a92ec3b461c590
Rodolfo Neto <rodolfotiago@gmail.com>
2019-04-01 12:09:12 -0300

    Update build-test.sh

--- cloud/docker/build-test.sh
+++ cloud/docker/build-test.sh
@@ -4,6 +4,7 @@
 
 IMAGE=flutter-android-sdk
 project_name=$1
+travis_flag=$2
 base_script=$(dirname $0)
 project_root=$base_script/../..
 create=''
@@ -33,7 +34,14 @@ if [[ $create == yes ]]; then
 	docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/ $IMAGE flutter create $project_name
 fi
 
-echo "testing the app..."
-echo "on project: $project_name"
-echo "docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name $IMAGE flutter test"
-docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name $IMAGE flutter test
+if [[ -z $travis_flag ]]; then
+	echo "testing the app..."
+	echo "on project: $project_name"
+	echo "docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name $IMAGE flutter test"
+	docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name $IMAGE flutter test
+else
+	echo "Building the app..."
+	echo "on project: $project_name"
+	echo "docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name $IMAGE flutter build apk"
+	docker run --rm -it -v $PWD/$project_name:/root/$project_name -w /root/$project_name $IMAGE flutter build apk
+fi
