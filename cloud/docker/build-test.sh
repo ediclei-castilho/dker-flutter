0af7b3c9b81a496daa0c2d80ecc824ee052ff065
Rodolfo Neto <rodolfotiago@gmail.com>
2019-04-01 11:35:58 -0300

    Update build-test.sh
    [skip ci]

--- cloud/docker/build-test.sh
+++ cloud/docker/build-test.sh
@@ -8,21 +8,22 @@ base_script=$(dirname $0)
 project_root=$base_script/../..
 create=''
 
+#if project variable not populated
 if [[ -z $project_name ]]; then
 	echo "give the name of the project as parameter"
 	exit 1
 fi
-
+#if project is new then create
 if [[ ! -d $project_root/$project_name ]]; then
 	echo "creating $project_root/$project_name"
 	mkdir $project_root/$project_name
 fi
+#if projct is empty then tell docker to create
 if [[ $(find $project_root/$project_name -name 'pubspec.yaml' | wc -l) == 1 ]]; then
 	create=no
 else
 	create=yes
 fi		
-
 
 echo "create project: $create"
 docker build --quiet -t $IMAGE .
